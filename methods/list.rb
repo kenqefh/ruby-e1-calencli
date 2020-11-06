ef1dcbbc1f53c6170772cc379cf60b18384d6e7e
Elvis Frank Dominguez Vidal <elvisf.dominguez@gmail.com>
2020-11-05 22:08:49 -0500

    list method almost is done, it works using methods to take and sort events data and print them

--- methods/list.rb
+++ methods/list.rb
@@ -1,28 +1,61 @@
 @days = %w[sunday monday tuesday wednesday thursday friday saturday]
 
 def list(year, week_munber)
+  # get all events for week
   data_week = @events[year][week_munber]
 
-  date_sort(data_week)
+  # sort elements by day and end_date
+  data_week = get_events_by_day(data_week)
+
+  # Print calenCLI
+  just_space = 15
+  line_break = "\n"
+  puts "#{'-' * 35}CalenCLI#{'-' * 35}#{line_break * 2}"
+  print_days(data_week, just_space, line_break)
 end
 
-def date_sort(data_week)
-  week_by_day = {}
-  @days.each do |day|
-    week_by_day[day] = data_week.select { |event| event[:start_date].strftime("%A").downcase.eql?(day) }
+def get_events_by_day(data_week)
+  @days.each_with_object({}) do |day, accumulator|
+    accumulator[day] = []
+
+    if data_week.is_a?(Array)
+      accumulator[day] = data_week.select { |event| event[:start_date].strftime("%A").downcase.eql?(day) }
+      sort_events_by_day(accumulator[day])
+    end
+    accumulator
   end
-  print_list(week_by_day)
 end
 
-def print_list(week_by_day)
+def sort_events_by_day(events)
+  events.sort! { |event, next_event| event[:end_date].to_s <=> next_event[:end_date].to_s }
+end
+
+def print_days(week_by_day, just_space = 15, line_break = "\n")
   week_by_day.each do |day, events|
-    puts day.colorize(:blue)
-    if !events.empty?
-      events.map do |event|
-        puts "#{event[:start_date]} - #{event[:end_date]} #{event[:title]} (#{event[:id]})"
-      end
+    print_day_date(day, just_space)
+
+    if events.empty?
+      puts "#{' ' * just_space}No events#{line_break * 2}"
     else
-      puts "No events"
+      print_day_events(events, just_space, line_break)
     end
   end
+end
+
+def print_day_date(day_not_formatted, just_space)
+  day_formatted = day_not_formatted.ljust(just_space, " ")
+  print day_formatted.colorize(:blue)
+end
+
+def print_day_events(day_events, just_space, line_break)
+  day_events.map do |event|
+    hour = ""
+    if event[:end_date].is_a?(Date)
+      hour << event[:start_date].strftime("%H:%M") << " - " << event[:end_date].strftime("%H:%M")
+    end
+    hour = hour.ljust(just_space, " ")
+
+    print "#{hour}#{event[:title]} (#{event[:id]})#{line_break}#{' ' * just_space}"
+  end
+  print line_break
 end
