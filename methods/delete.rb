ca74edb239fefcc72a2628b5413701b6ef4e8b45
Elvis Frank Dominguez Vidal <elvisf.dominguez@gmail.com>
2020-11-06 10:56:37 -0500

    add validation in case id_event will be 0, then delete method ends there

--- methods/delete.rb
+++ methods/delete.rb
@@ -1,4 +1,7 @@
 def delete(id_event)
+  # validation in case idevent == 0, then it goes out from method
+  return if id_event.zero?
+
   @events.each do |_, weeks|
     weeks.each do |_, events|
       events.delete_if { |event| event[:id] == id_event }
