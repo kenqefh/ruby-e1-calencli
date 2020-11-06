74e37e465f6069180c3b3c0b24892c9b83c4ad1f
Elvis Frank Dominguez Vidal <elvisf.dominguez@gmail.com>
2020-11-06 11:06:00 -0500

    changed validation in delete method from zero to positive as valid id

--- methods/delete.rb
+++ methods/delete.rb
@@ -1,6 +1,6 @@
 def delete(id_event)
-  # validation in case idevent == 0, then it goes out from method
-  return if id_event.zero?
+  # validation in case id_event does not positive, then it goes out from method
+  return unless id_event.positive?
 
   @events.each do |_, weeks|
     weeks.each do |_, events|
