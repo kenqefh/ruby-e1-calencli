bd18fb173284a07e61861c099c4d33c21d5cb759
Robert <robert.tm2018@gmail.com>
2020-11-06 22:02:59 -0500

    modified in input_event, methods validate_hour,  split into 5 methods

--- methods/create_event.rb
+++ methods/create_event.rb
@@ -1,10 +1,10 @@
 # rubocop:disable Metrics/AbcSize
 
-def current_week(date)
+def current_year(date)
   date.year
 end
 
-def current_year(date)
+def current_week(date)
   date.cweek
 end
 
@@ -18,73 +18,69 @@ def validate_date(date)
   dates
 end
 
+# Start Validator Hour
+def validate_hour_compare(start_date, end_date)
+  if start_date <= end_date
+    valid = true
+    puts "se valido fechas"
+  else
+    puts "Cannot end before start "
+    # start_end = gets.chomp
+    # puts "vali: #{valid}"
+    valid = false
+  end
+  valid
+end
+
 def get_date_time(dates, time)
-  # p ">>entro a get_date_time :: #{dates}"
   DateTime.new(dates[:year], dates[:mon], dates[:mday], time[0], time[1], 0)
 end
 
-# def validate_hour_compare(start_date, end_date)
-#  p "validatehourcompare: #{start_date}"
-#    if start_date <= end_date
-#      valid = true
-#      puts "se valido fechas"
-#    else
-#      print"Cannot end before start\nstart_end: "
-#      start_end = gets.chomp
-#      # puts "vali: #{valid}"
-#      valid = false
-#    end
-#  [valid, start_end]
-# end
+def get_start_end_time(start_end)
+  if !start_end.empty?
+    horas = start_end.split(" ")
+    # rubocop:disable Style/SymbolProc
+    start_time = horas[0].split(":").map { |h| h.to_i }
+    end_time = horas[1].split(":").map { |h| h.to_i }
+    # rubocop:enable Style/SymbolProc
+  else
+    start_time = 0
+    end_time = 0
+  end
 
-def validate_format_time(start_end)
+  [start_time, end_time]
+end
+
+def validate_format_time
+  print "start_end: ".rjust(15, " ")
+  start_end = gets.chomp
   until start_end.length == 11 || start_end.empty?
-    print "Format: 'HH:MM HH:MM' or leave it empty\nstart_end: "
+    puts "Format: 'HH:MM HH:MM' or leave it empty"
+    print "start_end: ".rjust(15, " ")
     start_end = gets.chomp
   end
   start_end
 end
 
-# rubocop:disable Metrics/MethodLength
-def validate_hour(dates, start_end)
+def validate_hour(dates)
   valid = false
   until valid == true
-    start_end = validate_format_time(start_end)
-    if !start_end.empty?
-      horas = start_end.split(" ")
-      # rubocop:disable Style/SymbolProc
-      start_time = horas[0].split(":").map { |h| h.to_i }
-      end_time = horas[1].split(":").map { |h| h.to_i }
-      # rubocop:enable Style/SymbolProc
-    else
-      start_time = 0
-      end_time = 0
-    end
-    start_date = get_date_time(dates, start_time)
-    end_date   = get_date_time(dates, end_time)
-    # valid = validate_hour_compare(start_date, end_date)[0]
-    # start_end = validate_hour_compare(start_date, end_date)[1]# NOS QUEDAMOS ACA BUSCANDO VALIDAR las hroas
-    # p "valid: #{valid}"
-    if start_date <= end_date
-      valid = true
-      puts "se valido fechas"
-    else
-      print "Cannot end before start\nstart_end: "
-      start_end = gets.chomp
-      # puts "vali: #{valid}"
-      # valid = false
-    end
+    start_end = validate_format_time
+    start_end_time = get_start_end_time(start_end)
+    start_date = get_date_time(dates, start_end_time[0])
+    end_date   = get_date_time(dates, start_end_time[1])
+    valid = validate_hour_compare(start_date, end_date)
   end
   [start_date, end_date]
 end
-# rubocop:enable Metrics/MethodLength
+# End Validator Hour
 
 def input_date(input_date, checker)
   label = input_date
-  print "#{label}: "
+  print "#{label}: ".rjust(15, " ")
   input_date = gets.chomp
   while input_date == "" && checker
-    print "Cannot be blank\n#{label}: "
+    print "Cannot be blank\n#{label}: ".rjust(15, " ")
     # print "title: "
     input_date = gets.chomp
   end
@@ -96,24 +92,20 @@ def input_event
   dates = validate_date(date)
   ## title = input_date("title",true)
   ## calendar = input_date("calendar",false)
-  print "start_end: "
-  start_end = gets.chomp
-  date_start_end = validate_hour(dates, start_end)
+  date_start_end = validate_hour(dates)
   ## notes = input_date("notes",false)
   ## guests = input_date("guests",false)
-  num_week = date_start_end[0].cweek.to_s # Numero de semana
-  year = date_start_end[0].year.to_s
+  # year = date_start_end[0].year.to_s # se puede optimizar decalrando defrente en el hash de salida
+  # num_week = date_start_end[0].cweek.to_s # Numero de semana
   # p "start_end: #{start_end} , num_week: #{num_week}"
   hash_event = { id: @id += 1, start_date: date_start_end[0], title: "Prueba",
                  calendar: "tech", end_date: date_start_end[1], notes: "", guests: "" }
-  # [year, num_week,ash_event]
-  { year: year, num_week: num_week, hash_event: hash_event }
+  { year: current_year(date_start_end[0]), num_week: current_week(date_start_end[0]), hash_event: hash_event }
 end
 
 def create_event
   event = input_event
   # p "Event:: #{event}"
-  # p " #{event}"
   if @events.key?(:"#{event[:year]}")
     if @events[:"#{event[:year]}"].key?(:"#{event[:num_week]}")
       @events[:"#{event[:year]}"][:"#{event[:num_week]}"].push(event[:hash_event])
